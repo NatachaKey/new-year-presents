b581d12858de5220c208e1a94e9d5c531b59b4ac
Natacha Key <103755090+NatachaKey@users.noreply.github.com>
2022-10-04 00:26:41 +0200

    Update Presents.js

--- src/Presents.js
+++ src/Presents.js
@@ -1,12 +1,12 @@
 import { Component } from "react";
 import icon from './icon.png';
+import done from './done.png'
 
 export class Presents extends Component{
     state={
         userInput: "",
-        groceryList: []
-
-        
+        presentsList: [],
+        deleteList: [],    
     }
 
     onChangeEvent(e){
@@ -21,15 +21,25 @@ export class Presents extends Component{
         }
         else{
 
-        let listArray = this.state.groceryList;
+        let listArray = this.state.presentsList;
         listArray.push(input)
-        this.setState({groceryList:listArray,  userInput: ""})
+        this.setState({presentsList:listArray,  userInput: ""})
     }
 }
+    
+      deleteOne(input,index){
+        let listArray=this.state.presentsList;
+        let delArray=this.state.deleteList;
+        listArray.splice(index,1);
+        delArray.push(input)   
+        this.setState({presentsList:listArray})
+        this.setState({deleteList:delArray})
+    }
+    
 deleteItem(){
-    let listArray= this.state.groceryList;
+    let listArray= this.state.presentsList;
     listArray=[];
-    this.setState({groceryList: listArray})
+    this.setState({presentsList: listArray})
 }
 
     crossedWord(event){
@@ -57,9 +67,17 @@ deleteItem(){
             </div>
 
             <ul>
-                {this.state.groceryList.map((item, index)=>(
+                {this.state.presentsList.map((item, index)=>(
                
-                <li onClick={this.crossedWord} key={index}><img src={icon} width="35px" alt="icon"/> {item}</li>
+                <li onClick={this.crossedWord} key={index}><img src={icon} width="35px" alt="icon"/> {item}
+                <div>
+                   <img src={done} width='26px' alt='del' onClick={()=>this.deleteOne(item,index)}  />
+                 </div>
+
+
+
+
+</li>
                 ))}
             </ul>
             <div className="container">
